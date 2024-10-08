4a16ac34755274c91d0e97b286c4f46e21ff103d
AJI Suryawan <ajisuryawan123@gmail.com>
2024-10-08 10:27:16 +0700

    fix: [AS] add data

--- lib/TaskListView.dart
+++ lib/TaskListView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'TaskController.dart';
+import 'models/TaskModel.dart';
 
 class TaskListView extends StatelessWidget {
   final TaskController taskController = Get.put(TaskController());
@@ -13,30 +14,43 @@ class TaskListView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Task List'),
-        
       ),
-      body: Obx(() {
-        return ListView.builder(
-          itemCount: taskController.tasks.length,
-          itemBuilder: (context, index) {
-            final task = taskController.tasks[index];
-            return ListTile(
-              title: Text(task.title),
-              subtitle: Text(task.description),
-              trailing: Checkbox(
-                value: task.isCompleted,
-                onChanged: (value) {
-                  task.isCompleted = value!;
-                  taskController.updateTask(task);
-                },
-              ),
-              onLongPress: () {
-                taskController.deleteTask(task.id!);
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              TaskModel task = TaskModel(
+                title: "titleController.text",
+                description: "descriptionController.text",
+              );
+              taskController.addTask(task);
+            },
+            child: Text('Add Task'),
+          ),
+          Expanded(child: Obx(() {
+            return ListView.builder(
+              itemCount: taskController.tasks.length,
+              itemBuilder: (context, index) {
+                final task = taskController.tasks[index];
+                return ListTile(
+                  title: Text(task.title),
+                  subtitle: Text(task.description),
+                  trailing: Checkbox(
+                    value: task.isCompleted,
+                    onChanged: (value) {
+                      task.isCompleted = value!;
+                      taskController.updateTask(task);
+                    },
+                  ),
+                  onLongPress: () {
+                    taskController.deleteTask(task.id!);
+                  },
+                );
               },
             );
-          },
-        );
-      }),
+          }))
+        ],
+      ),
     );
   }
 }
